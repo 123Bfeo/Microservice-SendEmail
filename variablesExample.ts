d49dfd55274990e2c7ac4a8558c7be6f849dbb1d
123Bfeo <ldnr13@hotmail.com>
2024-04-05 21:08:52 -0500

    Archivo Example varaibles

---
+++ variablesExample.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+export const MAILTRAP_HOST = 'hot.smtt.mailtrap.io';
+export const MAILTRAP_PORT = 2020;
+export const MAILTRAP_USER = 'aee004795555a2be0';
+export const MAILTRAP_PASS = 'e121da656455970302e';
