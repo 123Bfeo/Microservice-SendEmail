e6ca6056b9040cd9f863c0d6095ca733aa440f43
123Bfeo <ldnr13@hotmail.com>
2024-04-05 21:19:44 -0500

    cambio variables

--- variables.ts
+++ variables.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-export const MAILTRAP_HOST = 'sandbox.smtp.mailtrap.io';
-export const MAILTRAP_PORT = 2525;
-export const MAILTRAP_USER = 'aee004698a2be0';
-export const MAILTRAP_PASS = 'e121da6970302e';
+export const MAILTRAP_HOST = 'hot.smtt.mailtrap.io';
+export const MAILTRAP_PORT = 2020;
+export const MAILTRAP_USER = 'aee004795555a2be0';
+export const MAILTRAP_PASS = 'e121da656455970302e';
--- variablesExample.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-export const MAILTRAP_HOST = 'hot.smtt.mailtrap.io';
-export const MAILTRAP_PORT = 2020;
-export const MAILTRAP_USER = 'aee004795555a2be0';
-export const MAILTRAP_PASS = 'e121da656455970302e';
